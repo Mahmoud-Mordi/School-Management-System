3f0009846de0c1cb5044ec120c0ff15b7f8846a3
Mahmoud-Mordi <mahmoud.mm.mordi@gmail.com>
2025-09-14 16:58:45 +0300

    create Model BaseEntity

--- Model/Teacher.cpp
+++ Model/Teacher.cpp
@@ -1,45 +1,17 @@
-#include <iostream>
-using namespace std;
-class teacher {
+#include "BaseEntity.cpp"
+
+class teacher : public BaseEntity {
     private :
-        int id;
-        string name;
-        int age;
-        string phonenumber;
         double salary;
 
     public :
-
-            //Setter
-        void setId(int id){
-            this->id=id;
-        }
-        void setName(string name){
-            this->name=name;
-        }
-        void setAge(int age){
-            this->age=age;
-        }
-        void setPhonenumber(string Phonenumber){
-            this->phonenumber=phonenumber;
-        }
+    
+             //Setter
         void setSalary(double salary){
             this->salary=salary;
         }
 
-            //Getter
-        int getId(){
-            return id;
-        }
-        string getName(){
-            return name;
-        }
-        int getAge(){
-            return age;
-        }
-        string getPhonenumber(){
-            return phonenumber;
-        }
+             //Getter
         double getsalary(){
             return salary;
         }
