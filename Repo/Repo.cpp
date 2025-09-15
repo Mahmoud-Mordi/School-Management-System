13d05b2de254cafaa46b4b367c1c546c081dafcd
Mahmoud-Mordi <mahmoud.mm.mordi@gmail.com>
2025-09-15 15:04:59 +0300

    Repo(Student - Course - Teacher)

---
+++ Repo/Repo.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+////////////////////////////// Student ////////////////////////////// 
+//interface StudentRepository
+class StudentRepository {
+
+};
+
+//StudentRepositoryImpl
+class StudentRepositoryImpl : public StudentRepository {
+
+};
+
+
+////////////////////////////// Course ////////////////////////////// 
+//interface CourseRepository
+class CourseRepository {
+
+};
+
+//CourseRepositoryImpl
+class CourseRepositoryImpl : public CourseRepository {
+
+};
+
+
+////////////////////////////// Teacher ////////////////////////////// 
+//interface TeacherRepository
+class TeacherRepository {
+
+};
+
+//TeacherRepositoryImpl
+class TeacherRepositoryImpl : public TeacherRepository {
+
+};
